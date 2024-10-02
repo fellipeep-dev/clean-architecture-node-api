ea8790004e32f5a9ac5f19f4176eea7faaeb6013
Fellipe Esteves Pereira <fellipeep2005@gmail.com>
2024-10-02 17:41:45 -0300

    add prismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './infra/database/prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
 })
 export class AppModule {}
